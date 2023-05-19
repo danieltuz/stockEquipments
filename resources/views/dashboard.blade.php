5fc32c83b84e5a565b342e1961fe82f74b428f5c
Ivan Alvarado <ivan.alvarado@serprogramador.es>
2023-05-19 11:55:14 -0600

    Actualizando dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
 <!--begin::Head-->
 <head><base href="">
     <meta charset="utf-8" />
-    <title>Stock | Equipos</title>
+    <title>Stock | Equipos | DIF Estatal</title>
     <meta name="description" content="Updates and statistics" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
